72c6e17b08d6c262f1fc5e3eb40f961a5a264fdd
lyneverless <2099541261@qq.com>
2015-11-12 01:19:03 -0400

    write_blog created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '37e2bf3cb5a26000bb9c9aa9ec9d00b7c789c470ab6ceb8f211681bd4ba5f5ec708487cb23448032816716b594d1e071baa6c8c8b7fdd827c40b4f14c8d24a11'
