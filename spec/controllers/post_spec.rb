608b5e694d1d7cd06cb9d3c9a9acc594d805fc67
lyneverless <2099541261@qq.com>
2015-11-19 10:39:04 -0400

    frank

---
+++ spec/controllers/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :controller do
+	context '#index' do
+
+	end
+
+	context '#new' do
+	end
+
+	context '#create' do
+	end
+
+	context '#destroy' do
+	end
+end
+
+
